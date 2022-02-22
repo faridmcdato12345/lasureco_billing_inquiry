d189fa0c11d747c8c9b6013355f743ba6765d9a9
Cogie <faridmcdato@gmail.com>
2022-02-22 14:52:46 +0800

    change fb redirect

--- config/services.php
+++ config/services.php
@@ -33,6 +33,6 @@ return [
     'facebook' => [
         'client_id' => env('FACEBOOK_CLIENT_ID'),
         'client_secret' => env('FACEBOOK_CLIENT_SECRET'),
-        'redirect' => 'http://localhost:8000/callback-url',
+        'redirect' => 'https://intense-earth-70295.herokuapp.com/',
     ],
 ];
